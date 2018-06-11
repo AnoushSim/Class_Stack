68dc46a498d3c6d141411e4b6e246f162f2caa98
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-06-11 11:29:02 +0400

    Create stackmain.cpp

---
+++ stackmain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main() {
+	Stack * p = new Stack;
+	p ->push(1);
+	p ->push(2);
+	p ->push(3);
+	p ->push(4);
+	p->pop();
+	p->pop();
+	p->push(5);
+	p->push(6);
+	p->pop();
+	p->push(7);
+	p->push(8);
+	p->showStack();
+}
